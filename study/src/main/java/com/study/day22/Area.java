7ddbe7fcc80d5b980e9fce9053c945323648c55d
tkjh2 <tkjh2@DESKTOP-F39E0PL>
2021-08-10 23:27:00 +0800

    s

--- study/src/main/java/com/study/day22/Area.java
+++ study/src/main/java/com/study/day22/Area.java
@@ -1,5 +1,7 @@
 package com.study.day22;
 
+//0805上午(第15堂) 1:14:05
+
 // 面積
 public interface Area {
 	// 計算 (長 = h, 寬 = w)
--- study/src/main/java/com/study/day22/AreaMain.java
+++ study/src/main/java/com/study/day22/AreaMain.java
@@ -1,10 +1,15 @@
 package com.study.day22;
 
+//0805上午(第15堂) 1:14:05
+
 public class AreaMain {
 
 	public static void main(String[] args) {
 		
 		Area area = new Area() {
+			
+			//0805上午(第15堂) 1:16:05 (自動生成)
+
 			@Override
 			public int calc(int h, int w) {
 				int result = h * w;

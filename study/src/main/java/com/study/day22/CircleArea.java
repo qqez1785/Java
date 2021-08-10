c313b47d3b41078ee9a924e26d2b82785efe39dd
tkjh2 <tkjh2@DESKTOP-F39E0PL>
2021-08-10 23:02:12 +0800

    f

--- study/src/main/java/com/study/day22/CircleArea.java
+++ study/src/main/java/com/study/day22/CircleArea.java
@@ -1,5 +1,7 @@
 package com.study.day22;
 
+//0805上午(第15堂) 55:55
+
 // 圓面積/球體積
 public interface CircleArea {
 	int AREA   = 1; // 面積
--- study/src/main/java/com/study/day22/SayHello.java
+++ study/src/main/java/com/study/day22/SayHello.java
@@ -1,5 +1,6 @@
 package com.study.day22;
 
+//0805上午(第15堂) 55:55
 public interface SayHello {
 	void hello(String name);
 }

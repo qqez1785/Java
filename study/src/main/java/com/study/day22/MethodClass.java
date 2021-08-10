2349349b6697bdf4ce62d2fec963a1eba0da4f20
tkjh2 <tkjh2@DESKTOP-F39E0PL>
2021-08-10 22:15:30 +0800

    j

--- study/src/main/java/com/study/day22/MethodClass.java
+++ study/src/main/java/com/study/day22/MethodClass.java
@@ -1,11 +1,15 @@
 package com.study.day22;
 
+//0805上午(第15堂) 
+
 class Job {
 	// 方法
 	void start() {
 		int year = 2021; // 方法區域變數
 		                 // 若此變數要給方法內部類別
 		                 // 則編譯器會在背後加上 final 來修飾 (Java8 : 系統固定是 final) 35:00 
+		
+		//  year = 2021 ; (無法修改) 37:00
 		// 方法內部類別
 		class Report {
 			void content() {
